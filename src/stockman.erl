073423efbf0a3ff3430b8507ab87de2d99461ce8
Bahman Movaqar <Bahman@BahmanM.com>
2021-02-18 16:30:03 -0800

    de-duplicate logic and validate input

--- src/stockman.erl
+++ src/stockman.erl
@@ -1,10 +1,9 @@
 -module(stockman).
+-compile(export_all).
+%%-export([main/1]).
 
--export([main/1]).
-
--record(invoice_line, { doc_no, customer, date, total, discount,
-                        line_no, product, qty, price, line_amt }).
--record(invoice, {doc_no, customer, date, discount, total, lines}).
+-record(invoice_line, { line_no, product, qty, price, line_amt }).
+-record(invoice, { doc_no, customer, date, discount, total, lines }).
 
 
 main([FilePath|[]]) ->
@@ -13,33 +12,56 @@ main([FilePath|[]]) ->
 
 process(FilePath) ->
     {ok, Contents} = file:read_file(FilePath),
-    [_Header | Lines] = string:split(Contents, "\n", all),
-    InvoiceLines = lists:map(fun line_to_invoiceline/1, Lines),
-    Invoices = lists:foldl(fun group_invoicelines/2, dict:new(), InvoiceLines),
+    [_Header | Lines] = lists:filter(fun(Line) -> not string:is_empty(Line) end,
+                                     string:split(Contents, "\n", all)),
+    Invoices = lists:foldl(fun process/2, dict:new(), Lines),
     lists:foreach(fun(DocNo) ->
                          pprint(dict:fetch(DocNo, Invoices))
                  end, dict:fetch_keys(Invoices)).
 
-line_to_invoiceline(Line) ->
+process(Line, Invoices) ->
     [DocNo, Customer, Date, Total, Discount, LineNo, Product, Qty, Price, LineAmt] = string:split(Line, ",", all),
-    #invoice_line{ doc_no = DocNo, customer = Customer, date = Date, total = Total,
-                   discount = Discount, line_no = LineNo, product = Product, qty = Qty,
-                   price = Price, line_amt = LineAmt }.
-
-group_invoicelines(#invoice_line{doc_no=DocNo}=InvoiceLine, Invoices) ->
+    InvoiceLine = new_invoice_line(LineNo, Product, Qty, Price, LineAmt),
     case dict:find(DocNo, Invoices) of
         {ok, #invoice{lines = Lines}=Invoice} ->
             dict:store(DocNo, Invoice#invoice{ lines = [InvoiceLine|Lines] }, Invoices);
         _ ->
-            #invoice_line{ customer = Customer, date = Date, discount = Discount, total = Total } = InvoiceLine,
-            Invoice = #invoice { doc_no = DocNo, customer = Customer, date = Date, discount = Discount, total = Total, lines = [InvoiceLine] },
-            dict:store(DocNo, Invoice, Invoices)
+            Invoice = new_invoice(DocNo, Customer, Date, Discount, Total),
+            dict:store(DocNo, Invoice#invoice{ lines = [InvoiceLine] }, Invoices)
+    end.
+
+new_invoice(DocNo, Customer, Date, Discount, Total) ->
+    try
+        #invoice{ doc_no = DocNo,
+                  customer = Customer,
+                  date = Date,
+                  discount = erlang:binary_to_integer(Discount),
+                  total = erlang:binary_to_float(Total),
+                  lines = [] }
+    catch
+        error:badarg -> throw({invalid_invoice, {DocNo, Customer, Date, Discount, Total}})
+    end.
+
+new_invoice_line(LineNo, Product, Qty, Price, LineAmt) ->
+    try
+        #invoice_line{ line_no = erlang:binary_to_integer(LineNo),
+                       product = Product,
+                       qty = erlang:binary_to_integer(Qty),
+                       price = erlang:binary_to_float(Price),
+                       line_amt = erlang:binary_to_float(LineAmt) }
+    catch
+        error:badarg -> throw({invalid_invoice_line, {LineNo, Product, Qty, Price, LineAmt}})
     end.
 
 pprint(#invoice{ doc_no = DocNo, customer = Customer, date = Date, discount = Discount, total = Total, lines = Lines }) ->
-    io:fwrite("~-15s~-20s~-15s~n", [DocNo, Customer, Date]),
-    lists:foreach(fun pprint/1, Lines),
-    io:fwrite("~20s~20s~n", [Discount, Total]);
+    io:fwrite("~n~-15s ~-20s ~15s~n", [DocNo, Customer, Date]),
+    SortedLines = lists:sort(
+                    fun(#invoice_line{ line_no = LineNo1 }, #invoice_line{ line_no = LineNo2 }) ->
+                            LineNo1 =< LineNo2
+                    end,
+                    Lines),
+    lists:foreach(fun pprint/1, SortedLines),
+    io:fwrite("~56s~n~20B ~35.2f~n", ["------", -Discount, Total]);
 
 pprint(#invoice_line{ line_no = LineNo, product = Product, qty = Qty, price = Price, line_amt = LineAmt }) ->
-    io:fwrite("~4s~-20s~5s~8s~8s~n", [LineNo, Product, Qty, Price, LineAmt]).
+    io:fwrite("~4B ~-20s ~6B ~10.2f ~12.2f~n", [LineNo, Product, Qty, Price, LineAmt]).
