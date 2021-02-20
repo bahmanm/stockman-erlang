10876fea63a362e4058ff1619bb8383183557e78
Bahman Movaqar <Bahman@BahmanM.com>
2021-02-19 16:27:54 -0800

    update formatting and pretty printing

--- src/stockman.erl
+++ src/stockman.erl
@@ -15,20 +15,28 @@ process(FilePath) ->
     [_Header | Lines] = lists:filter(fun(Line) -> not string:is_empty(Line) end,
                                      string:split(Contents, "\n", all)),
     Invoices = lists:foldl(fun process/2, dict:new(), Lines),
-    lists:foreach(fun(DocNo) ->
-                         pprint(dict:fetch(DocNo, Invoices))
-                 end, dict:fetch_keys(Invoices)).
+    lists:foreach(fun(DocNo) -> pprint(dict:fetch(DocNo, Invoices)) end,
+                  dict:fetch_keys(Invoices)).
 
-process(Line, Invoices) ->
-    [DocNo, Customer, Date, Total, Discount, LineNo, Product, Qty, Price, LineAmt] = string:split(Line, ",", all),
+process([DocNo, Customer, Date, Total, Discount,
+         LineNo, Product, Qty, Price, LineAmt],
+        Invoices
+       ) ->
     InvoiceLine = new_invoice_line(LineNo, Product, Qty, Price, LineAmt),
     case dict:find(DocNo, Invoices) of
-        {ok, #invoice{lines = Lines}=Invoice} ->
-            dict:store(DocNo, Invoice#invoice{ lines = [InvoiceLine|Lines] }, Invoices);
+        {ok, #invoice{ lines = Lines } = Invoice} ->
+            dict:store(DocNo,
+                       Invoice#invoice{ lines = [InvoiceLine|Lines] },
+                       Invoices);
         _ ->
             Invoice = new_invoice(DocNo, Customer, Date, Discount, Total),
-            dict:store(DocNo, Invoice#invoice{ lines = [InvoiceLine] }, Invoices)
-    end.
+            dict:store(DocNo,
+                       Invoice#invoice{ lines = [InvoiceLine] },
+                       Invoices)
+    end;
+
+process(Line, Invoices) ->
+    process(string:split(Line, ",", all), Invoices).
 
 new_invoice(DocNo, Customer, Date, Discount, Total) ->
     try
@@ -39,7 +47,8 @@ new_invoice(DocNo, Customer, Date, Discount, Total) ->
                   total = erlang:binary_to_float(Total),
                   lines = [] }
     catch
-        error:badarg -> throw({invalid_invoice, {DocNo, Customer, Date, Discount, Total}})
+        error:badarg -> throw({ invalid_invoice,
+                                { DocNo, Customer, Date, Discount, Total } })
     end.
 
 new_invoice_line(LineNo, Product, Qty, Price, LineAmt) ->
@@ -50,18 +59,25 @@ new_invoice_line(LineNo, Product, Qty, Price, LineAmt) ->
                        price = erlang:binary_to_float(Price),
                        line_amt = erlang:binary_to_float(LineAmt) }
     catch
-        error:badarg -> throw({invalid_invoice_line, {LineNo, Product, Qty, Price, LineAmt}})
+        error:badarg -> throw({ invalid_invoice_line,
+                                { LineNo, Product, Qty, Price, LineAmt } })
     end.
 
-pprint(#invoice{ doc_no = DocNo, customer = Customer, date = Date, discount = Discount, total = Total, lines = Lines }) ->
-    io:fwrite("~n~-15s ~-20s ~15s~n", [DocNo, Customer, Date]),
+pprint(#invoice{ doc_no = DocNo, customer = Customer, date = Date,
+                 discount = Discount, total = Total, lines = Lines }
+      ) ->
+    io:fwrite("~n[~-15s] [~-20s] [~15s]~n", [DocNo, Customer, Date]),
     SortedLines = lists:sort(
-                    fun(#invoice_line{ line_no = LineNo1 }, #invoice_line{ line_no = LineNo2 }) ->
+                    fun(#invoice_line{ line_no = LineNo1 },
+                        #invoice_line{ line_no = LineNo2 }
+                       ) ->
                             LineNo1 =< LineNo2
                     end,
                     Lines),
     lists:foreach(fun pprint/1, SortedLines),
     io:fwrite("~56s~n~20B ~35.2f~n", ["------", -Discount, Total]);
 
-pprint(#invoice_line{ line_no = LineNo, product = Product, qty = Qty, price = Price, line_amt = LineAmt }) ->
+pprint(#invoice_line{ line_no = LineNo, product = Product, qty = Qty,
+                      price = Price, line_amt = LineAmt }
+      ) ->
     io:fwrite("~4B ~-20s ~6B ~10.2f ~12.2f~n", [LineNo, Product, Qty, Price, LineAmt]).
