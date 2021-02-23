2dacf08b789c1e9a9c4ce2553fbe05023c995a56
Bahman Movaqar <Bahman@BahmanM.com>
2021-02-22 22:16:45 -0800

    find the most expensive invoice

--- src/query.erl
+++ src/query.erl
@@ -1,6 +1,6 @@
 -module(query).
 -include("./stockman.hrl").
--export([total_sales/1]).
+-export([total_sales/1, most_expensive_invoice/1]).
 
 total_sales(Invoices) ->
     dict:fold(
@@ -11,7 +11,22 @@ total_sales(Invoices) ->
       Invoices
      ).
 
+most_expensive_invoice(Invoices) ->
+    dict:fold(fun most_expensive_invoice/3, undefined, Invoices).
 
+most_expensive_invoice(_, Invoice, undefined) ->
+    Invoice;
+
+most_expensive_invoice(_,
+                       #invoice{total=Total}=Invoice,
+                       #invoice{total=ResultTotal}=Result
+                      ) ->
+    if
+        Total > ResultTotal ->
+            Invoice;
+        true ->
+            Result
+    end.
 
 %%%
 %%% tests
@@ -29,5 +44,15 @@ total_sales_test() ->
                   {"i3", #invoice{doc_no="i3", total=30.5}}]
                 ),
     60.5 = total_sales(Invoices).
+
+most_expensive_invoice_test() ->
+    undefined = most_expensive_invoice(dict:new()),
+
+    Invoices = dict:from_list(
+                 [{"i1", #invoice{doc_no="i1", total=10.0}},
+                  {"i2", #invoice{doc_no="i2", total=20.0}},
+                  {"i3", #invoice{doc_no="i3", total=30.5}}]
+                ),
+    #invoice{total=30.5} = most_expensive_invoice(Invoices).
 
 -endif.
